ffa159825ac43bf20fbb7d418c5f38c02ef0d82c
augusto aparicio <augustoaparicio1999@gmail.com>
2021-05-15 00:45:09 -0300

    trabajo terminado

---
+++ Assets/UIControlador.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIControlador : MonoBehaviour
+{
+    Player player;
+    Text distanceText;
+
+    GameObject Resultado;
+    Text DistanciaFinalTexto;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        distanceText = GameObject.Find("DistanceText").GetComponent<Text>();
+
+        DistanciaFinalTexto = GameObject.Find("DistanciaFinalTexto").GetComponent<Text>();
+        Resultado = GameObject.Find("Resultado");
+        Resultado.SetActive(false);
+    }
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+
+    void Update()
+    {
+        int distance = Mathf.FloorToInt(player.distance);
+        distanceText.text = distance + "m";
+
+        if (player.isDead)
+        {
+            Resultado.SetActive(true);
+            DistanciaFinalTexto.text = distance + " m";
+        }
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
